35c68a13cd8687887f38fe271835e1e9e4beff3a
Smriti <rajsimmi369@gmail.com>
2023-12-19 12:32:41 +0530

    2nd Update code

--- script.js
+++ script.js
@@ -3,20 +3,52 @@ let recentInput = []
 let formInputEl = document.getElementById("inputForm")
 const listofRecentEl = document.querySelector(".listofRecent")
 
-inputSearchEl.addEventListener("keydown",()=>{
-    // console.log(inputSearchEl)
-    if(inputSearchEl){
-       document.getElementById("closeSearch").style.display = "block"    
-    }
-    else{
 
-        document.getElementById("closeSearch").style.display = "none"
+
+
+inputSearchEl.addEventListener("keydown", () => {
+    console.log(inputSearchEl.value)
+    if (inputSearchEl.value) {
+
+        document.getElementById("closesearch").style.display = "block"
+
+    }
+    else {
+
+        document.getElementById("closesearch").style.display = "none"
     }
 })
 
-formInputEl.addEventListener("sumbit",(e)=>{
+
+formInputEl.addEventListener("submit" , (e) =>{
     e.preventDefault()
+    console.log( e, "hey there!")
 
-     recentInput.push( inputSearchEl.value)
-    console.log(recentInput)
+    let listofRecentHTMLEl = ""
+
+    recentInput.push(inputSearchEl.value)
+    console.log(e.value , "value")
+
+    if (recentInput.length > 0) {
+        for (let i = 0; i < recentInput.length; i++) {
+            listofRecentHTMLEl += `
+            <div class="recentItem">
+            <div class="recentIcon">
+                <img src="recent.jpeg">
+             
+             </div>
+            <p>${recentInput[i]}</p>
+          </div>
+        `
+        }
+
+        listofRecentEl.innerHTML = listofRecentHTMLEl
+
+    }
 })
+
+
+
+
+
+
