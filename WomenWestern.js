f745a071ca66a1ea006fe58c99ab8ed954ea5852
Smriti <rajsimmi369@gmail.com>
2023-12-20 13:34:36 +0530

    Product Catalogs

---
+++ WomenWestern.js
@@ -0,0 +1,20 @@
+const WomenWestern =[
+    {
+        heading :"Topwear",
+        
+         data : ["Tops", "Dresses","Sweaters", "Jumpsuits",]
+    },
+    {
+        heading :"Bottomwear",
+        data : ["Jeans","Jeggings", "Palazzos"," Shorts","Skirts",]
+    },
+    {
+        heading :"Innerwear",
+        data : ["Bras","Briefs",]
+    },
+    {
+        heading :"Sleepwear",
+        data : ["nightsuits","Babydolls"]
+    },
+   
+]
